3c6c3cc91bccc229b87bf37df957af47ad858ec4
Atpton <s5704062636047@email.kmutnb.ac.th>
2018-09-03 14:00:49 +0700

    1234

--- src/app/services/getToken.js
+++ src/app/services/getToken.js
@@ -13,6 +13,8 @@ var _constants = require('../utils/constants');
 
 var _functions = require('../utils/functions');
 
+const queryString = require('query-string');
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
@@ -30,7 +32,7 @@ var GetToken = exports.GetToken = function (_Service) {
        // this.setHeaders({'Content-Type':'application/json'});
         this.setHeaders({'Content-Type':'application/x-www-form-urlencoded'});
         console.log("Test");
-        this.setBody( JSON.stringify({
+        this.setBody( queryString.stringify({
                          //  'grant_type':'urn:ietf:params:oauth:grant-type:jwt-bearer',
                            'grant_type':'urn%3Aietf%3Aparams%3Aoauth%3Agrant-type%3Ajwt-bearer',
                            'timeout':10000,
